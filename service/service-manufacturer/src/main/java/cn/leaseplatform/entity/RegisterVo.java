36116e8dd46807f0afaed1060eb293dc1be4484a
yuan-shuxin <307214073@qq.com>
2021-03-30 21:09:16 +0800

    周二晚合并

--- service/service-manufacturer/src/main/java/cn/leaseplatform/entity/RegisterVo.java
+++ service/service-manufacturer/src/main/java/cn/leaseplatform/entity/RegisterVo.java
@@ -2,13 +2,22 @@ package cn.leaseplatform.entity;
 
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 @ApiModel(value = "注册对象",description = "注册对象")
 public class RegisterVo {
 
+    @ApiModelProperty(value = "商家名称")
     private String adminname;
+    @ApiModelProperty(value = "密码")
     private String password;
+
+    @ApiModelProperty(value = "手机号")
+    private String phone;
+
+    @ApiModelProperty(value = "验证码")
+    private String code;
 
 }
